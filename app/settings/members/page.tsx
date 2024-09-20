a2ef2fe49660def26bebaf4bbca4d8f7fc3f6263
willian <willian.cacoelho@gmail.com>
2024-09-20 17:39:28 -0300

    do not allow members see invites

--- app/settings/members/page.tsx
+++ app/settings/members/page.tsx
@@ -4,8 +4,30 @@ import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Invites } from "./_components/invites";
 import { Members } from "./_components/members";
+import { useSearchParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 
 export default function MembersPage() {
+  const searchParams = useSearchParams();
+  const workspace = searchParams.get("workspace");
+
+  const userQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetch("/api/user").then((res) => res.json()),
+  });
+
+  // const membersQuery = useQuery({
+  //   queryKey: ["workspace/members"],
+  //   queryFn: () =>
+  //     fetch(`/api/workspace/members/${workspace}`).then((res) => res.json()),
+  // });
+
+  const ownerQuery = useQuery({
+    queryKey: ["workspace/owner"],
+    queryFn: () =>
+      fetch(`/api/workspace/owner/${workspace}`).then((res) => res.json()),
+  });
+
   return (
     <main className="flex flex-col items-center p-10">
       <div className="w-full max-w-3xl space-y-5">
@@ -14,7 +36,12 @@ export default function MembersPage() {
         <Tabs defaultValue="members" className="space-y-10">
           <TabsList>
             <TabsTrigger value="members">Membros</TabsTrigger>
-            <TabsTrigger value="invites">Convites</TabsTrigger>
+            <TabsTrigger
+              disabled={userQuery?.data?.id != ownerQuery?.data?.id}
+              value="invites"
+            >
+              Convites
+            </TabsTrigger>
           </TabsList>
           <TabsContent value="members">
             <Members />
