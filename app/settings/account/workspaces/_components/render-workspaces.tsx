e6031ed2789b44a07e72c901b41f23737e2c37b5
willian <willian.cacoelho@gmail.com>
2024-09-22 13:07:49 -0300

    show workspace and role

---
+++ app/settings/account/workspaces/_components/render-workspaces.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
+import { LogOut, Trash2 } from "lucide-react";
+
+export function RenderWorkspaces() {
+  const workspaceQuery = useQuery({
+    queryKey: ["workspace"],
+    queryFn: () => fetch("/api/workspace").then((res) => res.json()),
+  });
+
+  const userQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetch("/api/user").then((res) => res.json()),
+  });
+
+  const isOwner = (workspace: any) => {
+    return workspace.owner === userQuery.data?._id;
+  };
+
+  const getRole = (workspace: any) => {
+    const role = workspace.members.find(
+      (member: any) =>
+        member.memberId.toString() === userQuery.data?._id.toString()
+    )?.role;
+
+    return role == "member" ? "Membro" : "Administrador";
+  };
+
+  return workspaceQuery.isPending ? (
+    <div className="space-y-3">
+      <Skeleton className="h-11 w-full justify-start gap-2 px-3 text-start" />
+      <Skeleton className="h-11 w-full justify-start gap-2 px-3 text-start" />
+      <Skeleton className="h-11 w-full justify-start gap-2 px-3 text-start" />
+    </div>
+  ) : (
+    workspaceQuery.data?.map((workspace: any) => (
+      <div className="flex items-center justify-between space-x-4">
+        <div className="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="/shad.png" />
+            <AvatarFallback>OM</AvatarFallback>
+          </Avatar>
+          <div>
+            <p className="text-sm font-medium leading-none">{workspace.name}</p>
+            <p className="text-sm text-muted-foreground">
+              {isOwner(workspace) ? "Proprietário" : getRole(workspace)}
+            </p>
+          </div>
+        </div>
+        {isOwner(workspace) ? (
+          <div className="flex items-center gap-2">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="group hover:border-red-500 hover:bg-red-50"
+                >
+                  <Trash2 className="size-4 group-hover:text-red-500" />
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Esta ação não pode ser desfeita. Isso excluirá
+                    permanentemente o membro de seu workspace.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                  <AlertDialogAction>Continuar</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        ) : (
+          <>
+            <div className="flex items-center gap-2">
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="group hover:border-red-500 hover:bg-red-50"
+                  >
+                    <LogOut className="size-4 group-hover:text-red-500" />
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      Ao sair, você perderá o acesso a este workspace. Esta ação
+                      não pode ser desfeita.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                    <AlertDialogAction>Continuar</AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
+          </>
+        )}
+      </div>
+    ))
+  );
+}
--- app/settings/account/workspaces/page.tsx
+++ app/settings/account/workspaces/page.tsx
@@ -8,11 +8,12 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from '@/components/ui/alert-dialog'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Button } from '@/components/ui/button'
-import { Separator } from '@/components/ui/separator'
-import { LogOut, Trash2 } from 'lucide-react'
+} from "@/components/ui/alert-dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { LogOut, Trash2 } from "lucide-react";
+import { RenderWorkspaces } from "./_components/render-workspaces";
 
 export default function WorkspacesPage() {
   return (
@@ -27,83 +28,8 @@ export default function WorkspacesPage() {
             eficiente.
           </span>
         </section>
-        <div className="flex items-center justify-between space-x-4">
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarImage src="/shad.png" />
-              <AvatarFallback>OM</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-sm font-medium leading-none">Anuntech</p>
-              <p className="text-sm text-muted-foreground">Administrador</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-2">
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="group hover:border-red-500 hover:bg-red-50"
-                >
-                  <Trash2 className="size-4 group-hover:text-red-500" />
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Esta ação não pode ser desfeita. Isso excluirá
-                    permanentemente o membro de seu workspace.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                  <AlertDialogAction>Continuar</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
-        </div>
-        <div className="flex items-center justify-between space-x-4">
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarImage src="/shad.png" />
-              <AvatarFallback>OM</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-sm font-medium leading-none">Watec Service</p>
-              <p className="text-sm text-muted-foreground">Usuário</p>
-            </div>
-          </div>
-          <div className="flex items-center gap-2">
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="group hover:border-red-500 hover:bg-red-50"
-                >
-                  <LogOut className="size-4 group-hover:text-red-500" />
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Você tem certeza?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Ao sair, você perderá o acesso a este workspace. Esta ação
-                    não pode ser desfeita.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                  <AlertDialogAction>Continuar</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
-        </div>
+        <RenderWorkspaces />
       </div>
     </main>
-  )
+  );
 }
