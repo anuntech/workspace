cd949ecc669fd31a298207b1bfd28606ae3c1b6a
willian <willian.cacoelho@gmail.com>
2024-09-26 20:18:13 -0300

    load iframe

--- app/(workspace)/service/[id]/page.tsx
+++ app/(workspace)/service/[id]/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function ServicePage({ params }: { params: { id: string } }) {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const workspace = searchParams.get("workspace");
 
   if (!params.id) {
     router.push("/");
     return;
   }
 
-  return <div>This page will contain the service</div>;
+  const applicationsQuery = useQuery({
+    queryKey: ["applications"],
+    queryFn: async () => {
+      const res = await fetch(`/api/applications/${workspace}`);
+      return res.json();
+    },
+  });
+
+  if (applicationsQuery.isLoading) {
+    return <div>Carregando...</div>;
+  }
+
+  const app = applicationsQuery.data.find((app: any) => app._id === params.id);
+  return (
+    <iframe
+      src={app.applicationUrl}
+      width="100%"
+      height="100%"
+      style={{ border: "none" }}
+      title="Roteiro Digital"
+    />
+  );
 }
