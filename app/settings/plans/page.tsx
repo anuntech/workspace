c93cdfde91757c13770a214f3fee06dac0d59ea4
willian <willian.cacoelho@gmail.com>
2024-10-25 17:42:17 -0300

    edit all header

--- app/settings/plans/page.tsx
+++ app/settings/plans/page.tsx
@@ -6,6 +6,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import {
   Breadcrumb,
   BreadcrumbItem,
+  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
@@ -14,14 +15,13 @@ import {
 export default function PlansPage() {
   return (
     <>
-      {" "}
       <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">Workspace</BreadcrumbPage>
+              <BreadcrumbLink href="#">Workspace</BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator className="hidden md:block" />
             <BreadcrumbItem>
