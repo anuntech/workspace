40d133fe20590e63cb21f06bb1784000a1b6b6c4
willian <willian.cacoelho@gmail.com>
2024-10-29 14:31:51 -0300

    fix build error

--- app-example/api/stripe/create-checkout/route.ts
+++ app-example/api/stripe/create-checkout/route.ts
@@ -40,20 +40,20 @@ export async function POST(req: NextRequest) {
 
     const { priceId, mode, successUrl, cancelUrl } = body;
 
-    const stripeSessionURL = await createCheckout({
-      priceId,
-      mode,
-      successUrl,
-      cancelUrl,
-      // If user is logged in, it will pass the user ID to the Stripe Session so it can be retrieved in the webhook later
-      clientReferenceId: user?._id?.toString(),
-      // If user is logged in, this will automatically prefill Checkout data like email and/or credit card for faster checkout
-      user,
-      // If you send coupons from the frontend, you can pass it here
-      // couponId: body.couponId,
-    });
+    // const stripeSessionURL = await createCheckout({
+    //   priceId,
+    //   mode,
+    //   successUrl,
+    //   cancelUrl,
+    //   // If user is logged in, it will pass the user ID to the Stripe Session so it can be retrieved in the webhook later
+    //   clientReferenceId: user?._id?.toString(),
+    //   // If user is logged in, this will automatically prefill Checkout data like email and/or credit card for faster checkout
+    //   user,
+    //   // If you send coupons from the frontend, you can pass it here
+    //   // couponId: body.couponId,
+    // });
 
-    return NextResponse.json({ url: stripeSessionURL });
+    // return NextResponse.json({ url: stripeSessionURL });
   } catch (e) {
     console.error(e);
     return NextResponse.json({ error: e?.message }, { status: 500 });
