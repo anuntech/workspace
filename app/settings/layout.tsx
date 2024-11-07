71b161de8324c624f03caed73e770846ce82c3d8
willian <willian.cacoelho@gmail.com>
2024-11-07 13:23:15 -0300

    remove bug

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -2,7 +2,6 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/next-auth";
 import config from "@/config";
 import { redirect } from "next/navigation";
-import { RedirectNoneWorkspace } from "@/libs/redirect-none-workspace";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "./_components/app-sidebar";
 
@@ -19,16 +18,12 @@ export default async function SettingsLayout({
 
   return (
     <SidebarProvider>
-      <RedirectNoneWorkspace>
-        <AppSidebar />
-        <SidebarInset className="bg-[#F4F4F5]">
-          <div className="flex flex-1 bg-white flex-col my-4 mr-4 p-4 pt-0 h-[98vh] rounded-2xl">
-            <main className="overflow-auto rounded-md  bg-white">
-              {children}
-            </main>
-          </div>
-        </SidebarInset>
-      </RedirectNoneWorkspace>
+      <AppSidebar />
+      <SidebarInset className="bg-[#F4F4F5]">
+        <div className="flex flex-1 bg-white flex-col my-4 mr-4 p-4 pt-0 h-[98vh] rounded-2xl">
+          <main className="overflow-auto rounded-md  bg-white">{children}</main>
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
