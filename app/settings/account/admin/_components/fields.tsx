071e0c8d2dad843cd299e9848769e044aaf1da3d
willian <willian.cacoelho@gmail.com>
2024-11-22 15:07:40 -0300

    funcional

--- app/settings/account/admin/_components/fields.tsx
+++ app/settings/account/admin/_components/fields.tsx
@@ -9,45 +9,56 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { PlusCircle, X } from "lucide-react";
+import { useState } from "react";
 
 export function FieldsDialog() {
+  const [field, setField] = useState([
+    {
+      key: "",
+      value: "",
+    },
+  ]);
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button className="max-w-52">Gerenciar campos</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[600px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Gerenciar campos</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
+        {field.map((f, index) => (
+          <div key={index} className="flex gap-2">
+            <div className="flex gap-4">
+              <Input
+                placeholder="Nome do campo"
+                id="key"
+                defaultValue={f.key}
+                className="col-span-3"
+              />
+              <Input
+                id="username"
+                defaultValue={f.value}
+                className="col-span-3"
+                placeholder="Link do campo"
+              />
+            </div>
+            <Button variant="destructive">
+              <X />
+            </Button>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-        </div>
+        ))}
+        <Button>
+          <span>Adicionar campo</span>
+          <PlusCircle />
+        </Button>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit">Salvar mudanças</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
