9b4b77c1281c00ba4872cfdb52bcbbf809915020
willian <willian.cacoelho@gmail.com>
2024-12-02 10:45:15 -0300

    remove help button

--- app/create-workspace/page.tsx
+++ app/create-workspace/page.tsx
@@ -15,11 +15,6 @@ export default async function CreateWorkspacePage() {
 
   return (
     <div className="flex h-screen flex-col">
-      <header className="flex justify-end px-8 py-5">
-        <Link href="/help" className="hover:underline">
-          Ajuda
-        </Link>
-      </header>
       <main className="flex flex-1 flex-col items-center justify-center gap-8">
         <Image
           src="/anuntech-logo.svg"
