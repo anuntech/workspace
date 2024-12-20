5ded41c01cda1dd6b712541ff9c17d6b1b07bafd
willian <willian.cacoelho@gmail.com>
2024-12-20 12:17:03 -0300

    remove text

--- app/(workspace)/page.tsx
+++ app/(workspace)/page.tsx
@@ -114,10 +114,6 @@ export default function WorkspacePage() {
           <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb>
             <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Plataforma</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
               <BreadcrumbItem>
                 <BreadcrumbPage>Dashboard</BreadcrumbPage>
               </BreadcrumbItem>
