914d52c897369928a54e7d0ba9bee637828e365d
willian <willian.cacoelho@gmail.com>
2025-01-01 16:35:17 -0300

    create set position route

---
+++ app/api/applications/[workspaceId]/set-positions/route.ts
@@ -0,0 +1,121 @@
+import config from "@/config";
+import connectMongo from "@/libs/mongoose";
+import { authOptions } from "@/libs/next-auth";
+import Applications from "@/models/Applications";
+import MyApplications from "@/models/MyApplications";
+import User from "@/models/User";
+import Workspace from "@/models/Workspace";
+import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+/**
+ *
+ * body example:
+ * [
+ *  {appId: "62d1b9a0c9c4d9b1f0b0a1a2", position: 0},
+ *  {appId: "62d1b9a0c9c4d9b1f0b0a1a3", position: 1},
+ *  {appId: "62d1b9a0c9c4d9b1f0b0a1a4", position: 2}
+ * ]
+ *
+ * */
+export async function POST(
+  request: Request,
+  { params }: { params: { workspaceId: string } }
+) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    await connectMongo();
+
+    const workspace = await Workspace.findById(params.workspaceId);
+
+    if (!workspace) {
+      return NextResponse.json(
+        { error: "Workspace not found" },
+        { status: 404 }
+      );
+    }
+
+    const body = await request.json();
+
+    if (!body || !Array.isArray(body)) {
+      return NextResponse.json({ error: "Body is invalid" }, { status: 400 });
+    }
+
+    const myApplications = await MyApplications.findOne({
+      workspaceId: params.workspaceId,
+    });
+
+    if (!myApplications) {
+      return NextResponse.json(
+        { error: "You do not have permission to set positions" },
+        { status: 403 }
+      );
+    }
+
+    const memberRole = workspace.members.find(
+      (member) => member.memberId.toString() === session.user.id.toString()
+    )?.role;
+
+    if (
+      memberRole !== "admin" &&
+      workspace.owner.toString() !== session.user.id
+    ) {
+      return NextResponse.json(
+        { error: "You do not have permission to uninstall this application" },
+        { status: 403 }
+      );
+    }
+
+    if (body.length != myApplications.allowedApplicationsId.length) {
+      return NextResponse.json({ error: "Body is invalid" }, { status: 400 });
+    }
+
+    const positions = body.map((app) => app.position);
+    const sortedPositions = [...positions].sort((a, b) => a - b);
+    const expectedPositions = Array.from(
+      { length: sortedPositions.length },
+      (_, i) => i
+    );
+
+    if (
+      sortedPositions.length !== expectedPositions.length ||
+      !sortedPositions.every((pos, index) => pos === expectedPositions[index])
+    ) {
+      return NextResponse.json(
+        { error: "Positions must be sequential starting from 0" },
+        { status: 400 }
+      );
+    }
+
+    for (const app of body) {
+      if (!app.appId || !app.position) {
+        return NextResponse.json({ error: "Body is invalid" }, { status: 400 });
+      }
+
+      const appPosition = myApplications.appPositions.find(
+        (a) => a.appId.toString() === app.appId
+      );
+      if (!appPosition) {
+        return NextResponse.json({ error: "App not found" }, { status: 400 });
+      }
+
+      const indexInPositions = myApplications.appPositions.findIndex(
+        (a) => a.appId.toString() === app.appId
+      );
+      if (indexInPositions === -1) {
+        return NextResponse.json({ error: "App not found" }, { status: 400 });
+      }
+
+      myApplications.appPositions[indexInPositions].position = app.position;
+    }
+
+    await myApplications.save();
+
+    return NextResponse.json(true);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: e?.message }, { status: 500 });
+  }
+}
