def5e4d8ace38691f2812aa85243d5a0d1466292
willian <willian.cacoelho@gmail.com>
2025-01-22 12:02:41 -0300

    fix basic link information validation

--- app/(workspace)/_components/sidebar/links-manager/basic-link-informations.tsx
+++ app/(workspace)/_components/sidebar/links-manager/basic-link-informations.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-unused-vars */
 import { useEffect } from "react";
 import { z } from "zod";
 import {
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -14,175 +15,175 @@ import { AvatarSelector } from "@/components/avatar-selector";
 import { base64ToBlob } from "@/lib/utils";
 
 interface GetPrincipalLinkProps {
-  data: LinkFormData;
-  updateFormData: (
-    section: keyof LinkFormData,
-    updates: Partial<LinkFormData[keyof LinkFormData]>
-  ) => void;
-  isSublink?: boolean;
-  setStepValidation: (isValid: boolean) => void;
+	data: LinkFormData;
+	updateFormData: (
+		section: keyof LinkFormData,
+		updates: Partial<LinkFormData[keyof LinkFormData]>
+	) => void;
+	isSublink?: boolean;
+	setStepValidation: (isValid: boolean) => void;
 }
 
 export function BasicLinkInformation({
-  data,
-  updateFormData,
-  isSublink,
-  setStepValidation,
+	data,
+	updateFormData,
+	isSublink,
+	setStepValidation,
 }: GetPrincipalLinkProps) {
-  const { title, link, type } = data.principalLink;
+	const { title, link, type } = data.principalLink;
 
-  const principalLinkSchema = z.object({
-    title: z.string().min(1, "Título é obrigatório"),
-    link: z.string().refine(
-      (value) => {
-        if (data.principalLink.type === "none") return true;
-        return value.length > 0 && /^https?:\/\/.*/.test(value);
-      },
-      {
-        message: "Link inválido",
-      }
-    ),
-  });
+	const principalLinkSchema = z.object({
+		title: z.string().min(1, "Título é obrigatório"),
+		link: z.string().refine(
+			(value) => {
+				if (data.principalLink.type === "none") return true;
+				return value.length > 0 && /^https?:\/\/.*/.test(value);
+			},
+			{
+				message: "Link inválido",
+			}
+		),
+	});
 
-  const handleAvatarChange = (avatar: {
-    value: string;
-    type: "image" | "emoji" | "lucide";
-  }) => {
-    const formData = new FormData();
+	const handleAvatarChange = (avatar: {
+		value: string;
+		type: "image" | "emoji" | "lucide";
+	}) => {
+		const formData = new FormData();
 
-    switch (avatar.type) {
-      case "image": {
-        const blob = base64ToBlob(avatar.value);
-        formData.append("icon", blob, "avatar.jpeg");
-        formData.append("iconType", avatar.type);
+		switch (avatar.type) {
+			case "image": {
+				const blob = base64ToBlob(avatar.value);
+				formData.append("icon", blob, "avatar.jpeg");
+				formData.append("iconType", avatar.type);
 
-        updateFormData("images", {
-          icon: formData,
-          imageUrlWithoutS3: URL.createObjectURL(blob),
-          emojiAvatar: "",
-          emojiAvatarType: avatar.type,
-        });
-        break;
-      }
-      case "emoji":
-      case "lucide":
-        formData.append("icon", avatar.value);
-        formData.append("iconType", avatar.type);
-        updateFormData("images", {
-          icon: formData,
-          imageUrlWithoutS3: "",
-          emojiAvatar: avatar.value,
-          emojiAvatarType: avatar.type,
-        });
-        break;
-    }
+				updateFormData("images", {
+					icon: formData,
+					imageUrlWithoutS3: URL.createObjectURL(blob),
+					emojiAvatar: "",
+					emojiAvatarType: avatar.type,
+				});
+				break;
+			}
+			case "emoji":
+			case "lucide":
+				formData.append("icon", avatar.value);
+				formData.append("iconType", avatar.type);
+				updateFormData("images", {
+					icon: formData,
+					imageUrlWithoutS3: "",
+					emojiAvatar: avatar.value,
+					emojiAvatarType: avatar.type,
+				});
+				break;
+		}
+	};
 
-    setStepValidation(true);
-  };
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			const validationResult = principalLinkSchema.safeParse(
+				data.principalLink
+			);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const validationResult = principalLinkSchema.safeParse(
-        data.principalLink
-      );
+			console.log(data.images);
 
-      setStepValidation(validationResult.success);
-    }, 0);
+			setStepValidation(validationResult.success && !!data.images.icon);
+		}, 0);
 
-    return () => clearTimeout(timer);
-  }, [data.principalLink]);
+		return () => clearTimeout(timer);
+	}, [data.principalLink, data.images]);
 
-  const handleChange = (field: string, value: string) => {
-    if (value == "none") {
-      updateFormData("principalLink", { link: "" });
-    }
+	const handleChange = (field: string, value: string) => {
+		if (value == "none") {
+			updateFormData("principalLink", { link: "" });
+		}
 
-    updateFormData("principalLink", { [field]: value });
-  };
+		updateFormData("principalLink", { [field]: value });
+	};
 
-  return (
-    <>
-      <DialogHeader>
-        <DialogTitle>
-          Adicionar {!isSublink ? "link principal" : "sublink"} ao aplicativo
-        </DialogTitle>
-        <DialogDescription>
-          Adicione o {!isSublink ? "link principal" : "sublink"} do aplicativo,
-          que será o link que o usuário irá usar para acessar a tela principal
-          do aplicativo.
-        </DialogDescription>
-      </DialogHeader>
-      <div className="grid gap-4">
-        <div>
-          <Label htmlFor="title">Ícone do link *</Label>
-          <div>
-            <AvatarSelector
-              data={
-                data.images.emojiAvatar
-                  ? {
-                      value: data.images.emojiAvatar,
-                      type: data.images.emojiAvatarType,
-                    }
-                  : null
-              }
-              className="w-[120px]"
-              emojiSize="4rem"
-              imageUrlWithoutS3={
-                data.images.imageUrlWithoutS3
-                  ? data.images.imageUrlWithoutS3
-                  : null
-              }
-              onAvatarChange={handleAvatarChange}
-            />
-          </div>
-        </div>
-        <div>
-          <Label htmlFor="title">Título *</Label>
-          <Input
-            id="title"
-            placeholder="Coloque um nome para o aplicativo"
-            value={title}
-            onChange={(e) => handleChange("title", e.target.value)}
-          />
-        </div>
-        <div>
-          <Label htmlFor="link">Link do aplicativo *</Label>
-          <Input
-            id="link"
-            placeholder="https://seu-aplicativo.com"
-            value={link}
-            onChange={(e) => handleChange("link", e.target.value)}
-            disabled={type === "none"}
-          />
-        </div>
-        <div className="space-y-4">
-          <Label htmlFor="type">
-            Tipo de {!isSublink ? "link" : "sublink"} *
-          </Label>
-          <RadioGroup
-            value={type}
-            onValueChange={(value) => handleChange("type", value)}
-            className="space-y-2"
-          >
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="none" id="r1" />
-              <Label htmlFor="r1">Nenhum</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="iframe" id="r2" />
-              <Label htmlFor="r2">Iframe</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="newWindow" id="r3" />
-              <Label htmlFor="r3">Nova janela</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="sameWindow" id="r3" />
-              <Label htmlFor="r3">Mesma janela</Label>
-            </div>
-          </RadioGroup>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<DialogHeader>
+				<DialogTitle>
+					Adicionar {!isSublink ? "link principal" : "sublink"} ao aplicativo
+				</DialogTitle>
+				<DialogDescription>
+					Adicione o {!isSublink ? "link principal" : "sublink"} do aplicativo,
+					que será o link que o usuário irá usar para acessar a tela principal
+					do aplicativo.
+				</DialogDescription>
+			</DialogHeader>
+			<div className="grid gap-4">
+				<div>
+					<Label htmlFor="title">Ícone do link *</Label>
+					<div>
+						<AvatarSelector
+							data={
+								data.images.emojiAvatar
+									? {
+											value: data.images.emojiAvatar,
+											type: data.images.emojiAvatarType,
+									  }
+									: null
+							}
+							className="w-[120px]"
+							emojiSize="4rem"
+							imageUrlWithoutS3={
+								data.images.imageUrlWithoutS3
+									? data.images.imageUrlWithoutS3
+									: null
+							}
+							onAvatarChange={handleAvatarChange}
+						/>
+					</div>
+				</div>
+				<div>
+					<Label htmlFor="title">Título *</Label>
+					<Input
+						id="title"
+						placeholder="Coloque um nome para o aplicativo"
+						value={title}
+						onChange={(e) => handleChange("title", e.target.value)}
+					/>
+				</div>
+				<div>
+					<Label htmlFor="link">Link do aplicativo *</Label>
+					<Input
+						id="link"
+						placeholder="https://seu-aplicativo.com"
+						value={link}
+						onChange={(e) => handleChange("link", e.target.value)}
+						disabled={type === "none"}
+					/>
+				</div>
+				<div className="space-y-4">
+					<Label htmlFor="type">
+						Tipo de {!isSublink ? "link" : "sublink"} *
+					</Label>
+					<RadioGroup
+						value={type}
+						onValueChange={(value) => handleChange("type", value)}
+						className="space-y-2"
+					>
+						<div className="flex items-center space-x-2">
+							<RadioGroupItem value="none" id="r1" />
+							<Label htmlFor="r1">Nenhum</Label>
+						</div>
+						<div className="flex items-center space-x-2">
+							<RadioGroupItem value="iframe" id="r2" />
+							<Label htmlFor="r2">Iframe</Label>
+						</div>
+						<div className="flex items-center space-x-2">
+							<RadioGroupItem value="newWindow" id="r3" />
+							<Label htmlFor="r3">Nova janela</Label>
+						</div>
+						<div className="flex items-center space-x-2">
+							<RadioGroupItem value="sameWindow" id="r3" />
+							<Label htmlFor="r3">Mesma janela</Label>
+						</div>
+					</RadioGroup>
+				</div>
+			</div>
+		</>
+	);
 }
