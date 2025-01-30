2728b159d9ee184d501985ba94f6336d10bd8d55
Willian Cavalcanti <willian.cacoelho@gmail.com>
2025-01-30 12:00:16 -0300

    Develop (#37)
    * test pipeline
    
    * fix: state of notification when workspace is invalid
    
    * link work
    
    * fix: refetch query after add new app
    
    * finish link redirection type
    
    * hotfix show link name
    
    * move favorite routes to api folder
    
    * fix: workspace selected on account page
    
    * fix: link when not exists type
    
    * set up stripe
    
    * fix docker compose
    
    * set up stripe price
    
    * set up develop domain
    
    * update domainName
    
    * fix change user role
    
    * remove unique
    
    * validate invitedMembersEmail and membersIds
    
    * finish logs
    
    * finish logs again
    
    * solve conflicts
    
    * feat: disable feature of favorite links while not working
    
    * logs with expires 120d
    
    * improve plans page
    
    * remove comments
    
    * fix link share manager sharing the same members
    
    ---------
    
    Co-authored-by: Alessandro Henrique Ramos <alessandro.henri.r@gmail.com>

--- app/api/applications/[workspaceId]/allow/route.ts
+++ app/api/applications/[workspaceId]/allow/route.ts
@@ -24,87 +24,85 @@ async function POSTHandler(
 		return NextResponse.json({ error: "Workspace not found" }, { status: 404 });
 	}
 
-	throw new Error("teste");
+	const memberRole = workspace.members.find(
+		(member) => member.memberId.toString() === session.user.id.toString(),
+	)?.role;
 
-	// const memberRole = workspace.members.find(
-	// 	(member) => member.memberId.toString() === session.user.id.toString(),
-	// )?.role;
+	if (
+		memberRole !== "admin" &&
+		workspace.owner.toString() !== session.user.id
+	) {
+		return NextResponse.json(
+			{ error: "You do not have permission to get this workspace invites" },
+			{ status: 403 },
+		);
+	}
 
-	// if (
-	// 	memberRole !== "admin" &&
-	// 	workspace.owner.toString() !== session.user.id
-	// ) {
-	// 	return NextResponse.json(
-	// 		{ error: "You do not have permission to get this workspace invites" },
-	// 		{ status: 403 },
-	// 	);
-	// }
+	const myApplications = await MyApplications.findOne({
+		workspaceId: params.workspaceId,
+	});
 
-	// const myApplications = await MyApplications.findOne({
-	// 	workspaceId: params.workspaceId,
-	// });
+	const body = await request.json();
+	const application = await Applications.findById(body.applicationId);
 
-	// const body = await request.json();
-	// const application = await Applications.findById(body.applicationId);
+	if (!application) {
+		return NextResponse.json(
+			{ error: "Application do not exists" },
+			{ status: 400 },
+		);
+	}
 
-	// if (!application) {
-	// 	return NextResponse.json(
-	// 		{ error: "Application do not exists" },
-	// 		{ status: 400 },
-	// 	);
-	// }
+	if (
+		(application.workspaceAccess == "buyable" ||
+			application.workspaceAccess == "rentable") &&
+		!workspace.boughtApplications?.find(
+			(id) => id.toString() === application.id.toString(),
+		)
+	) {
+		return NextResponse.json(
+			{ error: "You can't allow this application" },
+			{ status: 403 },
+		);
+	}
 
-	// if (
-	// 	(application.workspaceAccess == "buyable" ||
-	// 		application.workspaceAccess == "rentable") &&
-	// 	!workspace.boughtApplications?.find(
-	// 		(id) => id.toString() === application.id.toString(),
-	// 	)
-	// ) {
-	// 	return NextResponse.json(
-	// 		{ error: "You can't allow this application" },
-	// 		{ status: 403 },
-	// 	);
-	// }
+	if (
+		application.workspaceAccess == "premium" &&
+		workspace.plan != "premium" &&
+		!workspace.boughtApplications?.find(
+			(a) => a.toString() === application.id.toString(),
+		)
+	) {
+		return NextResponse.json(
+			{ error: "The workspace is not premium" },
+			{ status: 403 },
+		);
+	}
 
-	// if (
-	// 	application.workspaceAccess == "premium" &&
-	// 	workspace.plan != "premium" &&
-	// 	!workspace.boughtApplications?.find(
-	// 		(a) => a.toString() === application.id.toString(),
-	// 	)
-	// ) {
-	// 	return NextResponse.json(
-	// 		{ error: "The workspace is not premium" },
-	// 		{ status: 403 },
-	// 	);
-	// }
+	if (myApplications) {
+		const plan = await Plans.findOne({ name: workspace.plan || "free" });
+		if (myApplications.allowedApplicationsId.length >= plan.appsLimit) {
+			return NextResponse.json(
+				{ error: "Atingiu o limite máximo de aplicativos" },
+				{ status: 403 },
+			);
+		}
+		const alreadyInstalled = myApplications.allowedApplicationsId.find(
+			(appId) => appId.toString() === application._id.toString(),
+		);
+		if (!alreadyInstalled) {
+			myApplications.allowedApplicationsId.push(application._id);
+		}
 
-	// if (myApplications) {
-	// 	const plan = await Plans.findOne({ name: workspace.plan || "free" });
-	// 	if (myApplications.allowedApplicationsId.length >= plan.appsLimit) {
-	// 		return NextResponse.json(
-	// 			{ error: "Atingiu o limite máximo de aplicativos" },
-	// 			{ status: 403 },
-	// 		);
-	// 	}
-	// 	const alreadyInstalled = myApplications.allowedApplicationsId.find(
-	// 		(appId) => appId.toString() === application._id.toString(),
-	// 	);
-	// 	if (!alreadyInstalled) {
-	// 		myApplications.allowedApplicationsId.push(application._id);
-	// 	}
+		await myApplications.save();
+		return NextResponse.json(myApplications);
+	}
 
-	// 	await myApplications.save();
-	// 	return NextResponse.json(myApplications);
-	// }
+	await MyApplications.create({
+		workspaceId: params.workspaceId,
+		allowedApplicationsId: [new mongoose.Types.ObjectId(body.applicationId)],
+	});
 
-	// await MyApplications.create({
-	// 	workspaceId: params.workspaceId,
-	// 	allowedApplicationsId: [new mongoose.Types.ObjectId(body.applicationId)],
-	// });
-
-	// return NextResponse.json(myApplications);
+	return NextResponse.json(myApplications);
 }
 
 async function DELETEHandler(
