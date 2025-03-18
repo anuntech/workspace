a2ebec4eef9b25832c73c7c950148940ba357055
Alessandro Henrique Ramos <alessandro.henri.r@gmail.com>
2025-03-18 16:25:27 -0300

    feat: update logo

--- libs/next-auth.ts
+++ libs/next-auth.ts
@@ -115,7 +115,7 @@ export const authOptions: NextAuthOptionsExtended = {
 		colorScheme: config.colors.theme as "light" | "dark" | "auto",
 		// Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
 		// It will be used in the login flow to display your logo. If you don't add it, it will look faded.
-		logo: `/anuntech-icon-black.png`,
+		logo: `/anuntech.png`,
 	},
 	pages: {
 		signIn: "/auth/sign-in",
