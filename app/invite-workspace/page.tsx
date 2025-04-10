2a5523e22f3c8963e859f8de1675c938fd69fb64
Alessandro Henrique Ramos <alessandro.henri.r@gmail.com>
2025-04-10 22:39:13 +0000

    chore: update paddings and intro

--- app/invite-workspace/page.tsx
+++ app/invite-workspace/page.tsx
@@ -23,11 +23,6 @@ export default function InviteWorkspacePage() {
 
 	return (
 		<div className="flex h-screen flex-col">
-			<header className="flex justify-end px-8 py-5">
-				<Link href="/help" className="hover:underline">
-					Ajuda
-				</Link>
-			</header>
 			<main className="flex flex-1 flex-col items-center justify-center gap-8">
 				<Image
 					src="/anuntech.png"
